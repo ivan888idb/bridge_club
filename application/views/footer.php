e9dd88e0f593c8459ccb38e16ac281960b1c36a9
One <One@One-PC>
2015-12-01 21:54:55 +0100

    Initial Commit

---
+++ application/views/footer.php
@@ -0,0 +1,14 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+</main><!-- #site-content -->
+
+	<footer id="site-footer" role="contentinfo">
+            <h5>@JTZ</h5>
+	</footer><!-- #site-footer -->
+
+	<!-- js -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="resources/js/bootstrap.min.js"></script>
+	<script src="resources/js/script.js"></script>
+
+</body>
+</html>
